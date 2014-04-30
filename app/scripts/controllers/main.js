d47aaa2890454edc9f4eb6b2143c37bc78856df6
Janas Page <jp@janaspage.com>
2014-04-30 15:16:51 -0700

    Code cleanup.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,47 +2,35 @@
 
 var app = angular.module('feedApp');
 
-app.directive('scrollBottom', function($window) {
-  function link(scope, el){
+app.directive('scrollBottom', function($window) { // MOVE DIRECTIVES TO SEPARATE FILE?
+
+  var link = function(){
     var feed = $window.document.getElementById('feed');
     feed.scrollTop = feed.scrollHeight + 44;
-    feed.scrollTop = 9999;
-  }
+  };
+
   return {
     link: link
   };
+
 });
 
-app.controller('MainCtrl', function($scope, $http, $document, $window, $location, $rootScope, $anchorScroll) {
-
-  // $scope.gotoBottom = function (){
-  //   // set the location.hash to the id of
-  //   // the element you wish to scroll to.
-  //   $location.hash('bottom');
-
-  //   // call $anchorScroll()
-  //   $anchorScroll();
-  // };
+app.controller('MainCtrl', function($scope, $http) {
 
   $http.get('/api/chat').success(function(chats) {
     $scope.chats = chats;
   });
 
-  $scope.sendChat = function(chat){
+  $scope.sendChat = function(chat) {
 
-    $http.post('/api/chat',
-      {user:chat.name, body:chat.body});
+    $http.post('/api/chat', {
+      user: chat.name,
+      body: chat.body
+    });
 
     $http.get('/api/chat').success(function(chats) {
       $scope.chats = chats;
-
-      $location.hash("")
-      // var feed = document.getElementById('feed');
-      feed.scrollTop = feed.scrollHeight + 44;
-      feed.scrollTop = 9999;
-      // debugger;
-
-    })
+    });
 
   };
 
