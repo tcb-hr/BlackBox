acceb86a9ea44d78ed5fb8a020b636ab524ec440
Katie <kah4dd@virginia.edu>
2014-04-30 20:06:57 -0700

    Basic leaflet map (and commented out Instagram/Sockets)

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -23,8 +23,51 @@ app.controller('MainCtrl', function($scope, $http) {
     body.scrollTop = body.scrollHeight + 44;
 
   };
+  
+  $scope.layer;
+  $scope.map;
+  $scope.marker;
+
+
+  $scope.createMap = function(){
+    $scope.layer = new L.StamenTileLayer("toner");
+    $scope.map = new L.Map("map", {
+      center: new L.LatLng(37.7, -122.4),
+      zoom: 12
+    });
+    $scope.map.addLayer($scope.layer);
+    $scope.marker = L.marker([37.7, -122.4]).addTo($scope.map);
+    
+    $("#map").height($(window).height());
+    $scope.map.invalidateSize();
+    
+    $('#map').click(function(){
+      $('#map').hide();
+    });
+    $('#map').hide();
+  };
+
+
+  /* Sample address input
+   "drop_address": {
+              "city": "San Francisco",
+              "name": "Haus",
+              "zip": "94109-8140",
+              "floor": "",
+              "state": "CA",
+              "street_address": "565 Ellis St"
+          }
+  */ 
+  $scope.showMap = function(latLng){
+    if(latLng !== ''){
+      $scope.marker.setLatLng([50, 50]); //change to chat.latLng
+      console.log(chat);
+      $('#map').show();
+    }
+  };
 
 });
+  
 
 app.filter('searchFor', function(){
   return function(arr, searchString){
