037a4f72ff9e7373511486c4d723659d3544af22
Janas Page <jp@janaspage.com>
2014-05-05 15:48:10 -0700

    Slide-panel has just begun to work.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,6 +2,26 @@
 
 var app = angular.module('feedApp');
 
+app.directive('mySlideController', ['$swipe', function($swipe) {
+  return {
+    restrict: 'EA',
+    link: function(scope, ele, attrs, ctrl) {
+      debugger;
+      var startX, pointX;
+      $swipe.bind(ele, {
+        'start': function(coords) {
+          startX = coords.x;
+          pointX = coords.y;
+        }, 'move': function(coords) {
+          var delta = coords.x - pointX;
+        }, 'end': function(coords) {
+        }, 'cancel': function(coords) {
+        }
+      });
+    }
+  };
+}]);
+
 app.directive('scrollBottom', function($window) { // MOVE DIRECTIVES TO A SEPARATE FILE?
 
   var scrollBottomWrap = function() {
@@ -25,6 +45,11 @@ app.directive('scrollBottom', function($window) { // MOVE DIRECTIVES TO A SEPARA
 });
 
 app.controller('MainCtrl', function($scope, $http, $window) {
+
+  $scope.showmenu = false;
+  $scope.toggleMenu = function() {
+    $scope.showmenu = ($scope.showmenu) ? false : true;
+  };
 
   var toolsVisible = false;
   $scope.showTools = function() {
--- app/scripts/controllers/navbar.js
+++ app/scripts/controllers/navbar.js
@@ -2,27 +2,52 @@
 
 var app = angular.module('feedApp');
 
-app.controller('NavbarCtrl', function ($scope, $location, Auth) {
-    $scope.menu = [{
-      'title': 'Dashboard',
-      'link': '/dashboard'
-    }, {
-      'title': 'Twinjet',
-      'link': '/'
-    }, {
-      'title': 'Settings',
-      'link': '/settings'
-    }];
-    
-    $scope.logout = function() {
-      Auth.logout()
-      .then(function() {
-        $location.path('/login');
-      });
-    };
-    
-    $scope.isActive = function(route) {
-      return route === $location.path();
-    };
-  });
+// app.directive('mySlideController', ['$swipe', function($swipe) {
+//   return {
+//     restrict: 'EA',
+//     link: function(scope, ele, attrs, ctrl) {
+//       debugger;
+//       var startX, pointX;
+//       $swipe.bind(ele, {
+//         'start': function(coords) {
+//           startX = coords.x;
+//           pointX = coords.y;
+//         }, 'move': function(coords) {
+//           var delta = coords.x - pointX;
+//         }, 'end': function(coords) {
+//         }, 'cancel': function(coords) {
+//         }
+//       });
+//     }
+//   };
+// }]);
 
+app.controller('NavbarCtrl', function ($scope, $location, Auth) {
+  $scope.menu = [{
+    'title': 'Dashboard',
+    'link': '/dashboard'
+  }, {
+    'title': 'Twinjet',
+    'link': '/'
+  }, {
+    'title': 'Settings',
+    'link': '/settings'
+  }];
+  
+  $scope.logout = function() {
+    Auth.logout()
+    .then(function() {
+      $location.path('/login');
+    });
+  };
+
+  $scope.isActive = function(route) {
+    return route === $location.path();
+  };
+
+  // $scope.showmenu = false;
+  // $scope.toggleMenu = function() {
+  //   $scope.showmenu = ($scope.showmenu) ? false : true;
+  // };
+
+});
