39d1c9592c4f2799b10559bbd6acb3eea53bd69b
AlexFarioletti <alex@tcbcourier.com>
2014-05-09 08:05:53 -0700

    chat breakout test

--- app/scripts/controllers/chat.js
+++ app/scripts/controllers/chat.js
@@ -3,10 +3,10 @@
 var app = angular.module('feedApp');
 
 app.controller('ChatCtrl', function($scope, $http, $window) {
-  $scope.$on('flow::complete', function (event, $flow, flowFile) {
-    console.log('we writing, yo');
-    $http.get('/download');
-  });
+  // $scope.$on('flow::complete', function (event, $flow, flowFile) {
+  //   console.log('we writing, yo');
+  //   $http.get('/download');
+  // });
 
   $scope.layer;
   $scope.map;
