1df869096a1f4ef2b9a08409c67c9db9bb5fbaba
AlexFarioletti <alex@tcbcourier.com>
2014-05-13 09:56:28 -0700

    farerek

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -459,10 +459,10 @@ app.controller('MainCtrl', function($scope, $http, $window, socket) {
     //
     //-------------------------------------------------
 
-    $scope.chats = [{
+    $scope.chats = {{
       _id: '999999999999999999999999',
       timestamp: '2024-05-12T21:52:56.361Z'
-    }];
+    }};
 
     $scope.fetchChats = function() {
       var chat = $scope.chats[1] || $scope.chats[0];
