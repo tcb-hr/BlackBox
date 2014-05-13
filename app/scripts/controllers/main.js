8a36c35dfb7cb5593f567146c8c1ec3cbb28960d
AlexFarioletti <alex@tcbcourier.com>
2014-05-12 23:08:50 -0700

    fuckin fetch

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -460,7 +460,8 @@ app.controller('MainCtrl', function($scope, $http, $window, socket) {
     //-------------------------------------------------
 
     $scope.chats = [{
-      _id: '999999999999999999999999'
+      _id: '999999999999999999999999',
+      timestamp: '2024-05-12T21:52:56.361Z'
     }];
 
     $scope.fetchChats = function() {
