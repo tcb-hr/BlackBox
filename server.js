d168ab2bb3784b2207d1511f3fb2529df21e8419
AlexFarioletti <alex@tcbcourier.com>
2014-05-14 17:49:19 -0700

    debug

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ io.sockets.on('connection', function (socket) {
   
   socket.on('hello', function(){
     var d = new Date();
-    d.setDate(d.getDate()-0.5);
+    d.setDate(d.getDate()-0.25);
     d.setTime(d.getTime()-d.getHours()*3600*1000-d.getMinutes()*60*1000);
     console.log('hello', d)
-    var chatStream = Chat.chatModel.find().where('timestamp').gt(d).tailable().stream();
+    var chatStream = Chat.chatModel.find().where('timestamp').gt(d).limit(15).tailable().stream();
     chatStream.on('data', function (chat) { 
       socket.emit('newMessage', {data: chat});
     }).on('error', function(err) {
