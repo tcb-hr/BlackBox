b508d19689f12acd8fa838baff89723a26c56377
AlexFarioletti <alex@tcbcourier.com>
2014-05-25 00:19:15 -0700

    standings client & server

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -401,7 +401,7 @@ app.controller('MainCtrl', function($scope, $http, $window, socket, $location, A
     //-------------------------------------------------
 
     $scope.chats = {};
-    $scope.racers = [];
+    $scope.racers = {};
     $scope.leader = {
       id: 1234,
       timestamp: Date.now,
