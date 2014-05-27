f782fcd82aca53f459394efba3b61976a98bcb92
AlexFarioletti <alex@tcbcourier.com>
2014-05-27 18:17:58 -0500

    info circle

--- server.js
+++ server.js
@@ -40,9 +40,24 @@ var server = app.listen(config.port, config.ip, function () {
 });
 
 // Sockets
-var io = require('socket.io').listen(server, {
-  'log level': 1
-});
+var io = require('socket.io').listen(server);
+
+io.enable('browser client minification');  // send minified client
+io.enable('browser client etag');          // apply etag caching logic based on version number
+io.enable('browser client gzip');          // gzip the file
+io.set('log level', 1);                    // reduce logging
+
+// enable all transports (optional if you want flashsocket support, please note that some hosting
+// providers do not allow you to create servers that listen on a port different than 80 or their
+// default port)
+io.set('transports', [
+    'websocket'
+  , 'flashsocket'
+  , 'htmlfile'
+  , 'xhr-polling'
+  , 'jsonp-polling'
+]);
+
 var Chat = require('./lib/models/chat');
 var Racer = require('./lib/models/racer');
 var Thing = require('./lib/models/thing');
